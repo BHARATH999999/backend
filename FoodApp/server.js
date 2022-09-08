151cf66ea62aec457f1e60fda6fdf2d4072184e2
BHARATH <bharathdarling970070@gmail.com>
2022-09-08 19:19:50 +0530

    Schema Added and Model Created

--- FoodApp/server.js
+++ FoodApp/server.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const app = express();
 const port = 3000;
-
+app.use(express.json())
 const userModel = require("./userModel");
+
+app.post('/signup',async function (req,res) {
+     let data = req.body;
+     let newUser = await userModel.create(data);
+     console.log(newUser);
+     res.json({
+        message : "Data received successfully",
+        data : data
+     })
+})
 
 app.listen(port,function(){
     console.log(`server started at port ${port}`)
