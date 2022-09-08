79ac63d4ffb19718569b7878f672080b10725fed
BHARATH <bharathdarling970070@gmail.com>
2022-09-08 19:25:03 +0530

    error management using try and catch

--- FoodApp/server.js
+++ FoodApp/server.js
@@ -5,13 +5,16 @@ app.use(express.json())
 const userModel = require("./userModel");
 
 app.post('/signup',async function (req,res) {
-     let data = req.body;
-     let newUser = await userModel.create(data);
-     console.log(newUser);
-     res.json({
-        message : "Data received successfully",
-        data : data
-     })
+    try{
+        let data = req.body;
+        let newUser = await userModel.create(data);
+        console.log(newUser);
+        res.json({
+            message : "Data received successfully"
+        })}
+    catch(err){
+        res.send(err.message)
+    }
 })
 
 app.listen(port,function(){
